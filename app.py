968c6c6d6b2618ed1c6e5a2e1dda48d5396a473e
VoiceOutReach <hssnchoudhary@gmail.com>
2025-06-09 23:15:54 +0500

    app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ I have a candidate for the {hiring_for_job_title} role. Keep it under 60 words.
 """, height=160)
 else:
  template = st.text_area("Template Message", value="""
-Hi {first_name}, I saw you're hiring for {hiring_for_job_title} at {company_name}.
+Hi {first_name}, I saw you+ "re"+ hiring for {hiring_for_job_title} at {company_name}.
 {job_description} Let's connect!
 """, height=150)
 
